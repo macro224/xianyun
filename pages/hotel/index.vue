09ae3ec0f152ca5303b53e2189d14c7cc2b7f7e9
Macro <macro224@163.com>
2019-12-30 15:31:39 +0800

    1-0 初始化项目代码，文件。

---
+++ pages/hotel/index.vue
@@ -0,0 +1,14 @@
+﻿<template>
+    <div>
+        机票模块首页文件    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ pages/post/index.vue
@@ -0,0 +1,15 @@
+﻿<template>
+    <div>
+        旅游攻略模块首页文件
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
